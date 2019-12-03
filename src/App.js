e2e8f3bdbf1a476094f9b7a0eff692eb60664151
Dai Nguyen <nvdai2401@gmail.com>
2019-12-03 23:31:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          How to Learn React 2019
         </a>
       </header>
     </div>
